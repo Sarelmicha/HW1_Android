f0c379a9a3098d7c6a61e40a0ccb5007867a783d
Sarel <sarelmicha93@gmail.com>
2019-11-24 20:01:27 +0200

    FixBugs

--- app/src/main/java/com/example/hw_sarelmicha/MainActivity.java
+++ app/src/main/java/com/example/hw_sarelmicha/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +21,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +31,17 @@ public class MainActivity extends AppCompatActivity {
     private final int NUM_OF_PICS = 3;
     private final int MAX_ENEMIES = 2;
     private final int MIN_ENEMIES = 0;
+    final int MIN_DURATION = 4000;
+    final int MAX_DURATION = 9000;
     private int numOfLife = 3;
     private int score = 0;
     private View[] enemies;
     private int[] enemiesPics;
     private View[] life;
+    private View jellyFish;
     private LinearLayout[] cols;
     private ValueAnimator[] animations;
+    private Handler handler;
     private RelativeLayout mainLayout;
     private RelativeLayout leftScreen;
     private RelativeLayout rightScreen;
@@ -43,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private int screenHeight;
     private static int animationIndex;
     private MediaPlayer ouchSound;
+    private MediaPlayer biteSound;
     private TextView scoreView;
 
     @Override
@@ -57,16 +66,58 @@ public class MainActivity extends AppCompatActivity {
         leftScreen = (RelativeLayout) findViewById(R.id.left_screen);
         rightScreen = (RelativeLayout) findViewById(R.id.right_screen);
         ouchSound = MediaPlayer.create(this, R.raw.ouchsound);
+        biteSound = MediaPlayer.create(this, R.raw.bite);
         scoreView = (TextView) findViewById(R.id.score);
-
-
-
+        handler = new Handler();
         addClickListeners();
         addEnemiesPics();
         addEnemies(NUM_OF_COLS);
         addLife();
-        setUpAnimations();
+        setUpEnemiesAnimations();
 
+
+    }
+
+    @Override
+    protected void onResume() {
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+              addJellyfish();
+                handler.postDelayed(this, 30000);
+            }
+        }, 30000);
+
+        super.onResume();
+        for (int i = 0; i < NUM_OF_COLS; i++) {
+            animations[i].resume();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for (int i = 0; i < NUM_OF_COLS; i++) {
+            animations[i].pause();
+        }
+    }
+
+    private void addJellyfish() {
+
+        jellyFish = new View(this);
+        RelativeLayout.LayoutParams params = new  RelativeLayout.LayoutParams(150, 150);
+        params.rightMargin = (int)(Math.random() * (((screenWidth - 50) - 50) + 1));
+        params.bottomMargin = 60;
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        jellyFish.setLayoutParams(params);
+        jellyFish.setBackgroundResource(R.drawable.jellyfish);
+        jellyFish.setAnimation(fadeInEffect());
+
+        mainLayout.addView(jellyFish);
     }
 
     private void addEnemiesPics() {
@@ -77,21 +128,12 @@ public class MainActivity extends AppCompatActivity {
         enemiesPics[2]= R.drawable.plastic2;
     }
 
-    private void setUpAnimations() {
-
-        final int MIN_DURATION = 4000;
-        final int MAX_DURATION = 9000;
-        final float initialHeight = -300;
+    private void setUpEnemiesAnimations() {
 
         animations = new ValueAnimator[NUM_OF_COLS];
-        
 
         for (animationIndex = 0; animationIndex < NUM_OF_COLS; animationIndex++) {
-
-            animations[animationIndex] = ValueAnimator.ofFloat(initialHeight, screenHeight);
-            animations[animationIndex].setDuration(MAX_DURATION + (long)(Math.random() * (MAX_DURATION - MIN_DURATION)));
-            animations[animationIndex].setStartDelay((long) (Math.random() * (MIN_DURATION)));
-            animations[animationIndex].setRepeatCount(Animation.INFINITE);
+            setAnimationParameters();
             animations[animationIndex].addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
                 private int x = animationIndex;
@@ -104,16 +146,9 @@ public class MainActivity extends AppCompatActivity {
                     enemies[x].setTranslationY(animatedValue);
                     enemies[x].setVisibility(View.VISIBLE);
                     if (isCollide(enemies[x])) {
-                        makeOuchSound();
-                        updatedAnimation.setStartDelay(0);
-                        updatedAnimation.start();
-                        reduceLife();
-                    } else if (enemies[x].getY() >= screenHeight - 1) { //Enemy is Out of Screen
-                        updateScore();
-                        updatedAnimation.setDuration(MIN_DURATION + (long)(Math.random() * (MAX_DURATION - MIN_DURATION)));
-                        updatedAnimation.setStartDelay((long) (Math.random() * (1000)));
-                        enemies[x].setBackgroundResource(enemiesPics[(int)(Math.random() * ((MAX_ENEMIES - MIN_ENEMIES) + 1))]);
-                        updatedAnimation.start();
+                        collideWithEnemyOccurred(updatedAnimation);
+                    } else if (isOutOfHeightScreen(enemies[x])) {
+                        enemyIsOutOfScreen(updatedAnimation,x);
                     }
                 }
             });
@@ -122,17 +157,66 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void setAnimationParameters(){
+
+        final float initialHeight = -300;
+
+        animations[animationIndex] = ValueAnimator.ofFloat(initialHeight, screenHeight);
+        animations[animationIndex].setDuration(MAX_DURATION + (long)(Math.random() * (MAX_DURATION - MIN_DURATION)));
+        animations[animationIndex].setStartDelay((long) (Math.random() * (MIN_DURATION)));
+        animations[animationIndex].setRepeatCount(Animation.INFINITE);
+    }
+
+    private boolean isOutOfWidthScreen(View view){
+        if(view.getX() >= screenWidth - 1)
+            return true;
+        return false;
+    }
+
+    private boolean isOutOfHeightScreen(View view){
+        if(view.getY() >= screenHeight - 1)
+            return true;
+        return false;
+    }
+
+    private void enemyIsOutOfScreen(ValueAnimator updatedAnimation,int x){
+        updateScore();
+        updatedAnimation.setDuration(MIN_DURATION + (long)(Math.random() * (MAX_DURATION - MIN_DURATION)));
+        updatedAnimation.setStartDelay((long) (Math.random() * (1000)));
+        enemies[x].setBackgroundResource(enemiesPics[(int)(Math.random() * ((MAX_ENEMIES - MIN_ENEMIES) + 1))]);
+        updatedAnimation.start();
+    }
+
+    private void collideWithEnemyOccurred(ValueAnimator updatedAnimation){
+
+        makeOuchSound();
+        updatedAnimation.setStartDelay(0);
+        updatedAnimation.start();
+        reduceLife();
+    }
+
+    private void collideWithJellyfishOccurred(){
+
+        jellyFish.setVisibility(View.INVISIBLE);
+       jellyFish = null;
+       makeBiteSound();
+        deduceLife();
+    }
+
     private void makeOuchSound() {
         ouchSound.start();
     }
 
+    private void makeBiteSound(){
+        biteSound.start();
+
+    }
     private void addLife() {
         life = new View[numOfLife];
 
         life[0] = (View) findViewById(R.id.life0);
         life[1] = (View) findViewById(R.id.life1);
         life[2] = (View) findViewById(R.id.life2);
-
     }
 
     void setScreenHeightAndWidth() {
@@ -146,28 +230,10 @@ public class MainActivity extends AppCompatActivity {
         screenHeight = size.y;
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        for (int i = 0; i < NUM_OF_COLS; i++) {
-            animations[i].resume();
-        }
-    }
-
     private synchronized void updateScore() {
 
         score++;
         scoreView.setText("Score:" + score);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        for (int i = 0; i < NUM_OF_COLS; i++) {
-            animations[i].pause();
-        }
     }
 
     @Override
@@ -180,11 +246,7 @@ public class MainActivity extends AppCompatActivity {
 
     private synchronized void reduceLife() {
 
-        //Fade Out effect
-        Animation fadeOut = new AlphaAnimation(1, 0);
-        fadeOut.setInterpolator(new AccelerateInterpolator());
-        fadeOut.setStartOffset(0);
-        fadeOut.setDuration(500);
+        Animation fadeOut = fadeOutEffect();
 
         numOfLife--;
         life[numOfLife].setAnimation(fadeOut);
@@ -192,6 +254,40 @@ public class MainActivity extends AppCompatActivity {
 
         if (numOfLife == 0)
             endGame();
+
+    }
+
+    private Animation fadeInEffect(){
+
+        //Fade In effect
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator());
+        fadeIn.setStartOffset(0);
+        fadeIn.setDuration(500);
+
+        return fadeIn;
+    }
+
+    private Animation fadeOutEffect(){
+
+        //Fade Out effect
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(0);
+        fadeOut.setDuration(500);
+
+        return fadeOut;
+    }
+
+    private synchronized void deduceLife() {
+
+        Animation fadeIn = fadeInEffect();
+
+        if(numOfLife == 3)
+            return;
+        life[numOfLife].setAnimation(fadeIn);
+        life[numOfLife].setVisibility(View.VISIBLE);
+        numOfLife++;
 
     }
 
@@ -241,9 +337,12 @@ public class MainActivity extends AppCompatActivity {
                 if (player.getX() > 0){
                     player.setX(player.getX() - 50);
                     player.setBackgroundResource(R.drawable.playerright);
+                    if(jellyFish != null){
+                        if(isCollide(jellyFish))
+                            collideWithJellyfishOccurred();
+                    }
+
                 }
-
-
             }
         });
 
@@ -253,6 +352,10 @@ public class MainActivity extends AppCompatActivity {
                 if (player.getX() < screenWidth - player.getWidth()){
                     player.setX(player.getX() + 50);
                     player.setBackgroundResource(R.drawable.playerleft);
+                    if(jellyFish != null){
+                        if(isCollide(jellyFish))
+                            collideWithJellyfishOccurred();
+                    }
                 }
             }
         });
